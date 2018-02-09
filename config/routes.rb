5ac3ad3aa0adc36f0b5c11350b4a9f18293d8c5f
ErikPerez312 <theerikperez@gmail.com>
2018-02-08 19:25:34 -0800

    Forgot to commit the routes haha

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  resources :searches
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   'home#index'
 end
